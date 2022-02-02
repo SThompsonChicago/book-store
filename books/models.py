4fde2a4247d05176f20a1cab09d1156007b9d778
Stephen Thompson <s31@umbc.edu>
2022-02-02 10:33:27 -0600

    added shopping cart and order classes

--- books/models.py
+++ books/models.py
@@ -11,3 +11,34 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     publication_year = models.IntegerField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    inventory = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    complete = models.BooleanField(default=False)
+    placed_at = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    book = models.ForeignKey(Book, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=7, decimal_places=2)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
